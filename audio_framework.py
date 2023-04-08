9cdd8e3b424285affc6ede90deb34f39a9f7bf34
123connorpoop <rct3fan55@yahoo.com>
2023-04-08 16:42:22 -0400

    Create multiple ear training function

--- audio_framework.py
+++ audio_framework.py
@@ -5,6 +5,7 @@ from scipy.signal import find_peaks
 import time
 import threading
 import pygame.midi as midi
+from mingus.containers import Note
 notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 def get_pitch(signal):
@@ -26,7 +27,9 @@ def pitch_to_note(pitch):
     octave = pitch_index // 12 - 1
     
     return note + str(octave)
-def match_note(target_note):
+def match_note(target_note, duration):
+    c = Note()
+    c.from_int(target_note)
     p = pyaudio.PyAudio()
     stream = p.open(format=pyaudio.paFloat32, channels=1, rate=44100, input=True, frames_per_buffer=2048)
     note_held_time = 0.0
@@ -36,31 +39,38 @@ def match_note(target_note):
         pitch = get_pitch(signal)
         note = pitch_to_note(pitch)
         if pitch > 0:
-            if note == target_note:
+            if note == c.name + str(c.octave):
                 note_held_time += 1024/44100 # increment the note held time by the buffer length
-                if note_held_time >= 2.0:
+                if note_held_time >= duration:
                     break # exit the loop if the note has been held for more than two seconds
             elif note_held_time > 0:
                 note_held_time -= 1024/44100 # decrement the note held time if the note changes
-        print(note, note_held_time)
-    print("Note held!")
+       # print(note, note_held_time)
+    return True
 
 def midi_quit():
     del player
     midi.quit()
 
 def play_note_helper(player, note):
-    time.sleep(1)
     player.note_off(note, 127)
     
 def play_note(note):
-    player.note_on(note, 127)
-    timer = threading.Timer(1, play_note_helper, [player, note])
+    player.note_on(note + 24, 127) # the pygame midi is offset by two octaves for some reason
+    timer = threading.Timer(1, play_note_helper, [player, note + 24])
     timer.start()
-def ear_train(note):
-    play_note(60)
-    match_note("C4")
+def ear_train(note, duration):
+    
+    play_note(note)
+    match_note(note, duration)
+def ear_train_multiple(notes):
+    for note in notes:
+        play_note(note)
+        time.sleep(1)
+    for note in notes:
+        match_note(note, 0.5)
 #midi setup
 midi.init()
 player = midi.Output(midi.get_default_output_id())
 player.set_instrument(0)
+ear_train_multiple([24, 26, 28, 29, 31])
