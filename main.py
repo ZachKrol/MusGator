371db833f4e3346da727fa32bd3212a340240bcc
Taylor Frederick <tfrederick1@ufl.edu>
2023-04-14 15:05:14 -0400

    Add quiz page, update lesson to support 2 vers

--- main.py
+++ main.py
@@ -33,11 +33,14 @@ class HomePage(QMainWindow):
         self.sightLearnIndex = 3
         self.rhythmLearnIndex = 4
         self.earLearnIndex = 5
+
+        self.noteQuizIndex = 6
         
         self.learnNotes.clicked.connect(lambda: self.toNoteLearn(pages))
         self.learnSight.clicked.connect(lambda: self.toSightLearn(pages))
         self.learnRhythm.clicked.connect(lambda: self.toRhythmLearn(pages))
         self.learnEar.clicked.connect(lambda: self.toEarLearn(pages))
+        self.quizNotes.clicked.connect(lambda: self.toNoteQuiz(pages))
 
     def toNoteLearn(self, pages):
         pages.setCurrentIndex(self.noteLearnIndex)
@@ -51,14 +54,17 @@ class HomePage(QMainWindow):
     def toEarLearn(self, pages):
         pages.setCurrentIndex(self.earLearnIndex)
 
+    def toNoteQuiz(self, pages):
+        pages.setCurrentIndex(self.noteQuizIndex)
 
 
-# Learn Page
-class LearnPage(QMainWindow):
+
+# Learn Page 1
+class LearnPage1(QMainWindow):
     def __init__(self, pages, lesson_name):
-        super(LearnPage, self).__init__()
+        super(LearnPage1, self).__init__()
         
-        loadUi("Learning.ui", self)
+        loadUi("note-rythLesson.ui", self)
 
         self.text = []
         self.title = []
@@ -133,6 +139,157 @@ class LearnPage(QMainWindow):
             self.lessonImage.setScene(scene)
 
 
+# Learn Page 2
+class LearnPage2(QMainWindow):
+    def __init__(self, pages, lesson_name):
+        super(LearnPage2, self).__init__()
+
+        loadUi("sight-earLesson.ui", self)
+
+        self.title = []
+        self.imagename = []
+
+        self.index = 0
+
+        with open('data.csv') as file:
+            csv_reader = csv.reader(file, delimiter=',')
+
+            for row in csv_reader:
+                    if row[0] == "Lesson":
+                        if row[1] == lesson_name:
+                            self.title.append(row[2])
+                            self.imagename.append(row[3])
+
+        self.previousButton.setText("Back")
+        self.lessonTitle.setText(lesson_name)
+        self.lessonTextLabel.setText(self.title[self.index])
+        
+        scene = QGraphicsScene()
+        scene.addPixmap(QPixmap("./images/" + self.imagename[self.index]))
+        self.lessonImage.setScene(scene)
+
+        self.previousButton.clicked.connect(lambda: self.clickPreviousButton(pages))
+        self.nextButton.clicked.connect(lambda: self.clickNextButton(pages))
+
+    def clickPreviousButton(self, pages):
+        if self.index == 0:
+            pages.setCurrentIndex(1) # Return to home page
+        
+        else:
+            if self.index == 1:
+                self.previousButton.setText("Back")
+            
+            self.index = self.index - 1
+            self.lessonTextLabel.setText(self.title[self.index])
+
+            scene = QGraphicsScene()
+            scene.addPixmap(QPixmap("./images/" + self.imagename[self.index]))
+            self.lessonImage.setScene(scene)
+
+    def clickNextButton(self, pages):
+        if self.index == len(self.text) - 1:
+            pages.setCurrentIndex(1) # Return to home page
+            self.nextButton.setText("Next")
+            self.index = 0
+
+            self.lessonTextLabel.setText(self.title[self.index])
+
+            scene = QGraphicsScene()
+            scene.addPixmap(QPixmap("./images/" + self.imagename[self.index]))
+            self.lessonImage.setScene(scene)
+        
+        else:
+            if self.index == len(self.text) - 2:
+                self.nextButton.setText("Finish")
+            
+            if self.index == 0:
+                self.previousButton.setText("Previous")
+
+            self.index = self.index + 1
+            self.lessonTextLabel.setText(self.title[self.index])
+
+            scene = QGraphicsScene()
+            scene.addPixmap(QPixmap("./images/" + self.imagename[self.index]))
+            self.lessonImage.setScene(scene)
+
+
+
+# Quiz Page
+class QuizPage(QMainWindow):
+    def __init__(self, pages, quiz_name, quiz_type):
+        super(QuizPage, self).__init__()
+        
+        if quiz_type == 1:
+            loadUi("note-rythQuiz.ui", self)
+        
+        elif quiz_type == 2:
+            loadUi("sight-earQuiz.ui", self)
+
+        self.text = []
+        self.imagename = []
+
+        self.index = 0
+
+        with open('data.csv') as file:
+            csv_reader = csv.reader(file, delimiter=',')
+
+            for row in csv_reader:
+                    if row[0] == "Quiz":
+                        if row[1] == quiz_name:
+                            self.text.append(row[2])
+                            self.imagename.append(row[3])
+
+        self.previousButton.setText("Back")
+        self.quizTitle.setText(quiz_name)
+        self.quizText.setText(self.text[self.index])
+        
+        scene = QGraphicsScene()
+        scene.addPixmap(QPixmap("./images/" + self.imagename[self.index]))
+        self.quizImage.setScene(scene)
+
+        self.previousButton.clicked.connect(lambda: self.clickPreviousButton(pages))
+        self.nextButton.clicked.connect(lambda: self.clickNextButton(pages))
+
+    def clickPreviousButton(self, pages):
+        if self.index == 0:
+            pages.setCurrentIndex(1) # Return to home page
+        
+        else:
+            if self.index == 1:
+                self.previousButton.setText("Back")
+            
+            self.index = self.index - 1
+            self.quizText.setText(self.text[self.index])
+
+            scene = QGraphicsScene()
+            scene.addPixmap(QPixmap("./images/" + self.imagename[self.index]))
+            self.quizImage.setScene(scene)
+
+    def clickNextButton(self, pages):
+        if self.index == len(self.text) - 1:
+            pages.setCurrentIndex(1) # Return to home page
+            self.nextButton.setText("Next")
+            self.index = 0
+
+            self.quizText.setText(self.text[self.index])
+
+            scene = QGraphicsScene()
+            scene.addPixmap(QPixmap("./images/" + self.imagename[self.index]))
+            self.quizImage.setScene(scene)
+        
+        else:
+            if self.index == len(self.text) - 2:
+                self.nextButton.setText("Finish")
+            
+            if self.index == 0:
+                self.previousButton.setText("Previous")
+
+            self.index = self.index + 1
+            self.quizText.setText(self.text[self.index])
+
+            scene = QGraphicsScene()
+            scene.addPixmap(QPixmap("./images/" + self.imagename[self.index]))
+            self.quizImage.setScene(scene)
 
 
 def main():
@@ -148,10 +305,16 @@ def main():
     splashPage = SplashPage(pages)
     homePage = HomePage(pages)
 
-    noteLearnPage = LearnPage(pages, "Note Learning")
-    sightLearnPage = LearnPage(pages, "Sight Reading")
-    rhythmLearnPage = LearnPage(pages, "Rhythm")
-    earLearnPage = LearnPage(pages, "Ear Training")
+    noteLearnPage = LearnPage1(pages, "Note Learning")
+    sightLearnPage = LearnPage2(pages, "Sight Reading")
+    rhythmLearnPage = LearnPage1(pages, "Rhythm")
+    earLearnPage = LearnPage2(pages, "Ear Training")
+
+    noteQuizPage = QuizPage(pages, "Note Quiz", 1)
+    noteQuizPage = QuizPage(pages, "Rhythm Quiz", 1)
+    noteQuizPage = QuizPage(pages, "Sight Reading Quiz", 1)
+    noteQuizPage = QuizPage(pages, "Ear Training Quiz", 1)
+
 
     pages.addWidget(splashPage) # index 0
     pages.addWidget(homePage) # index 1
@@ -160,6 +323,8 @@ def main():
     pages.addWidget(sightLearnPage) # index 3
     pages.addWidget(rhythmLearnPage) # index 4
     pages.addWidget(earLearnPage) # index 5
+
+    pages.addWidget(noteQuizPage) # index 6
 
     pages.show()
 
